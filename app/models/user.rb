8e8e06ae82b0654f341e18d397f501e872567381
okapyy <m9_17g_suki@yahoo.co.jp>
2020-04-27 12:39:03 +0900

    ユーザーの新規登録・ログイン機能（ヘッダーからはまだ飛べません）

---
+++ app/models/user.rb
@@ -0,0 +1,29 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      @user.update(birthday:birthday_join)
+      # redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:birthday)
+  end
+
+  def birthday_join
+    date = params[:user][:birthday]
+    if date["birthday(1i)"].empty? && date["birthday(2i)"].empty? && date["birthday(3i)"].empty?
+      return
+    end
+    Date.new date["birthday(1i)"].to_i,date["birthday(2i)"].to_i,date["birthday(3i)"].to_i
+  end
+end
