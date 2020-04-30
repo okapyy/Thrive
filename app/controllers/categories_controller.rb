c98b3b7c1d17b763b47fb8d973f7f6b72fc4f314
kfyuta <yuta.funai@gmail.com>
2020-04-30 16:43:47 +0900

    markup category index and show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,16 @@
 class CategoriesController < ApplicationController
   def index
     @parents = Category.all.order("id ASC").limit(13)
-    @lady = Category.find(1)
-    @lady_children = @lady.children
-    @ladies_item = Category.where(ancestry: "1/14") 
+    # @lady = Category.find(1)
+    # @lady_children = @lady.children
+    # @ladies_item = Category.where(ancestry: "1/14") 
 
-    @ladyChildren = @lady.children
+    # @ladyChildren = @lady.children
+  end
+
+  def show
+    params_id = params[:id].to_i - 1
+    @parents = Category.all.order("id ASC").limit(13)
+    @parent = @parents[params_id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'items#index'
-  get 'categories/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  
   resources :items
+  resources :categories, only: [:index, :show]
 
 end
