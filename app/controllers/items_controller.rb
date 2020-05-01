1be61cdb5ed283dd1e63cc471c18a30ab7f2dcb7
shota12085 <shota.toyoshima007@gmail.com>
2020-05-01 17:40:54 +0900

    ビューファイル変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,10 @@ class ItemsController < ApplicationController
   end
 
   def top
+    @ladys = Item.where(category_id: 1)
+    @mens = Item.where(category_id: 2)
+    @electrical = Item.where(category_id: 8)
+    @hobby = Item.where(category_id: 6)
   end
 
 end
