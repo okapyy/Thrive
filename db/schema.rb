537c90c933569c0f5cd9594b471ea75925edc3d2
okapyy <m9_17g_suki@yahoo.co.jp>
2020-05-04 16:52:05 +0900

    トップページcss修正

--- db/schema.rb
+++ db/schema.rb
@@ -28,17 +28,6 @@ ActiveRecord::Schema.define(version: 2020_04_28_084503) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
-end
-
-ActiveRecord::Schema.define(version: 2020_04_24_075352) do
-
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_addresses_on_user_id"
-  end
-end
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -73,7 +62,6 @@ end
     t.index ["nickname"], name: "index_users_on_nickname", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 
   add_foreign_key "addresses", "users"
   add_foreign_key "cards", "users"
