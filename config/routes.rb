37661b9728ede46a043c3ccafb211701e67a082f
kfyuta <yuta.funai@gmail.com>
2020-05-05 15:57:24 +0900

    修正をコミット

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,11 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
-  get 'top', to: 'items#top'
-
   resources :categories, only: [:index, :show]
   resources :items do
-   get 'buypage', to: 'items#buypage'
-   get 'list', to: 'items#list'
+    get 'top', to: 'items#top'  
+    get 'buypage', to: 'items#buypage'
+    get 'list', to: 'items#list'
   end
   resources :users, only: :show do
     get 'logout', to: 'users#logout'
