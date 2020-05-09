c00ff4cd9cb368b5bd194f25fbbe00b67e950ab7
kfyuta <yuta.funai@gmail.com>
2020-05-09 18:27:53 +0900

    error handling

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,8 +36,12 @@ class ItemsController < ApplicationController
   end
   
   def edit
-    @image = ItemImage.where(item_id: @item.id)
-    @count = @item.item_images.count
+    if current_user.id == @item.user_id
+      @image = ItemImage.where(item_id: @item.id)
+      @count = @item.item_images.count
+    else
+      redirect_to item_path(@item)
+    end
   end
   
   def update
@@ -70,15 +74,24 @@ class ItemsController < ApplicationController
   end
   
   def buypage
+    if current_user.id == @item.user_id
+      redirect_to item_path(@item)
+    elsif @item.is_deleted
+      redirect_to item_path(@item)
+    end
   end
 
   def buy
+    if @card.present?
     @item.update!(is_deleted: 1, buyer_id: current_user.id)
       charge = Payjp::Charge.create(
         amount: @item.price,
         customer: Payjp::Customer.retrieve(@card.customer_id),
         currency: 'jpy'
       )
+    else
+      render :buypage
+    end
   end
   
   private
@@ -91,30 +104,36 @@ class ItemsController < ApplicationController
   end
 
   def set_items
-    @item = Item.find(params[:id])
+    if params[:id].to_i  <= Item.all.length
+      @item = Item.find(params[:id])
+    else
+      redirect_to root_path
+    end
   end
 
   def set_card
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
     @card = Card.find_by(user_id: current_user.id)
-    customer = Payjp::Customer.retrieve(@card.customer_id)
-      @cards = customer.cards.retrieve(@card.card_id)
-      @brand = @cards.brand
-      case @brand
-      when "Visa"
-        @card_brand = "credit-card_visa.png"
-      when "MasterCard"
-        @card_brand = "credit-card_master.png"
-      when "JCB"
-        @card_brand = "credit-card_jcb.png"
-      when "American Express"
-        @card_brand = "credit-card_american.png"
-      when "Diners Club"
-        @card_brand = "credit-card_diners.png"
-      when "Discover"
-        @card_brand = "credit-card_discover.png"
-      end
-      @month = @cards.exp_month.to_s
-      @year = @cards.exp_year.to_s.slice(2,3)
+    if @card
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+        @cards = customer.cards.retrieve(@card.card_id)
+        @brand = @cards.brand
+        case @brand
+        when "Visa"
+          @card_brand = "credit-card_visa.png"
+        when "MasterCard"
+          @card_brand = "credit-card_master.png"
+        when "JCB"
+          @card_brand = "credit-card_jcb.png"
+        when "American Express"
+          @card_brand = "credit-card_american.png"
+        when "Diners Club"
+          @card_brand = "credit-card_diners.png"
+        when "Discover"
+          @card_brand = "credit-card_discover.png"
+        end
+        @month = @cards.exp_month.to_s
+        @year = @cards.exp_year.to_s.slice(2,3)
+    end
   end
 end
